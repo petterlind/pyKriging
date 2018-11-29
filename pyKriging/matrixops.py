1f60d84446f54dfd6bd00b87687820d074643b71
petterlind <petterlind2@gmail.com>
2018-11-29 09:49:20 +0100

    Bspline surf LS works, not impl.
    Bspline surface works, but is not yet implemented in the kriging model. - next step.
    
    Must rewrite self.F and self.beta in order to implement.

--- pyKriging/matrixops.py
+++ pyKriging/matrixops.py
@@ -1,10 +1,12 @@
 
 import numpy as np
 from numpy.matlib import rand,zeros,ones,empty,eye
+from geomdl import helpers
 import scipy
 import scipy.interpolate as interp
+import matplotlib.pyplot as plt
 import pdb
-
+from geomdl.visualization import VisMPL as vis
 
 class matrixops():
 
@@ -13,13 +15,117 @@ class matrixops():
         self.Psi = np.zeros((self.n, self.n), dtype=np.float)
         self.psi = np.zeros((self.n, 1))
         self.one = np.ones(self.n)
+        self.reg = None             # regression order
         self.beta = None
         self.mu = None
         self.U = None
         self.SigmaSqr = None
         self.Lambda = 1
         self.updateData()
+        
+    def compute_q(self, Bspl, x, y, R, dim):
+        
+        p_u = Bspl.degree_u
+        p_v = Bspl.degree_v
+        U = Bspl.knotvector_u
+        V = Bspl.knotvector_v
+        n_u = len(U) - p_u - 1  # 6st
+        n_v = len(V) - p_v - 1  # 6st
+        
+        B_row = None
+        for i in range(len(x)):
+        
+            [u, v] = x[i]  # parameterisation ?!
+        
+            span_u = helpers.find_span_binsearch(p_u, U, n_u, u)
+            f_nz_u = helpers.basis_function(p_u, U, span_u, u)
+            
+            Nu = np.zeros((n_u,))
+            Nu[(span_u - p_u): (span_u + 1)] = f_nz_u
+            
+            span_v = helpers.find_span_binsearch(p_v, V, n_v, v)
+            f_nz_v = helpers.basis_function(p_v, U, span_v, v)
+            
+            Nv = np.zeros((n_v,))
+            Nv[(span_v - p_v): (span_v + 1)] = f_nz_v
+            
+            # Following Nils Carlssons master thesis
+            A_conc = np.concatenate(np.outer(Nu, Nv))
+            
+            if B_row is None:
+                B_row = A_conc
+            else:
+                B_row = np.append(B_row, A_conc)  # one long row of data, Rewrite for speed?
+        
+        B = np.reshape(B_row, (-1, len(A_conc)))  # Sort up the control points in one vector
+        
+        # Controlpoint vec
+        p = []
+        for comp in Bspl.ctrlpts:
+            for in_comp in comp:
+                p.append(in_comp)
+        
+        Qdim = np.matmul(B, p[dim::3])
+        fun_val = np.stack((x[:, 0], x[:, 1], y), axis=-1)
+        
+        norm = np.dot(Qdim - fun_val[:, dim], np.dot(R, Qdim - fun_val[:, dim]))  # generalized least square
+        return norm
+        
+        
+    def update_bspl(self, Bspl, inp, dim):
+        
+        # WARNING hard coded stuff!
+        ctrlpts_u = 3
+        ctrlpts_v = 3
+
+        CP = []
+        for comp in Bspl.ctrlpts:
+            for in_comp in comp:
+                CP.append(in_comp)
+        
+        if dim == 2:
+            # Assign
+            CP[dim::3] = inp
+        else:
+            CP[12 + dim] = inp[5]
+        
+        
+        # Group
+        x = CP[0::3]
+        y = CP[1::3]
+        z = CP[2::3]
+        # Hard-code initial and end values. INTE BARA PÅ EN PLATS !
+        x[0:ctrlpts_u:] = np.zeros((ctrlpts_u,))
+        x[-ctrlpts_u:-1] = np.ones((ctrlpts_u,))
+        
+        y[0::3] = np.zeros((ctrlpts_v,))
+        y[2::3] = np.ones((ctrlpts_v,))
+        Bspl.ctrlpts = list(zip(x, y, z))
+        return Bspl
     
+    def controlPointsOpt(self, Bspl, X, Y, R):
+        '''
+        
+        TODO Test other algorithms,
+        analytical derivative
+        Rewrite fun as def for speed.
+        '''
+        for dim in range(0, 3):
+            x0 = []
+            for pt in Bspl.ctrlpts:
+                x0.append(pt[dim])
+            
+            fun = lambda x: self.compute_q(self.update_bspl(Bspl, x, dim), X, Y, R, dim)
+            opt_res = scipy.optimize.minimize(fun, x0, method='SLSQP', options={'disp': True})
+            if opt_res.success:
+                Bspl = self.update_bspl(Bspl, opt_res.x, dim)
+                
+            else:
+                print('optimization failed!')
+                raise ValueError
+        
+        return Bspl
+        
     def mean_f(self, x, type):
         if type is None or type == 'constant':
             n = len(X)
@@ -36,11 +142,6 @@ class matrixops():
         elif type == 'Third':
             raise NotImplementedError
             
-        elif type == 'Bspline':
-            # Only for surfaces in this form.
-            
-            raise NotImplementedError
-            
         else:
             print('Unknown trend function type')
             raise ValueError
@@ -52,18 +153,24 @@ class matrixops():
         self.distance = np.zeros((self.n, self.n, self.k))
         for i in range(self.n):
             for j in range(i + 1, self.n):
-                self.distance[i, j] = np.abs((self.X[i]-self.X[j]))  # Every position in this matrix is an array of dimension k!
+                self.distance[i, j] = np.abs((self.X[i] - self.X[j]))  # Every position in this matrix is an array of dimension k!
         
         F = []
-        for i in range(0, self.n):
-            F.append(self.mean_f(self.X[i], 'First').tolist())
-        
-        self.F = np.array(F)
-        
+        if self.reg != 'Bspline':
+            for i in range(0, self.n):
+                F.append(self.mean_f(self.X[i], 'First').tolist())
+            self.F = np.array(F)
+        elif self.reg == 'Bspline':
+            self.F
+            # Set F to NTN
+        else:
+            print('Unknown regression type, or reg type not set!')
+            raise ValueError
+            
     def updatePsi(self):
         self.Psi = np.zeros((self.n, self.n), dtype=np.float)
         self.one = np.ones(self.n)
-        self.psi = np.zeros((self.n,1))
+        self.psi = np.zeros((self.n, 1))
         newPsi = np.exp(-np.sum(self.theta*np.power(self.distance,self.pl), axis=2))
         self.Psi = np.triu(newPsi,1)
         self.Psi = self.Psi + self.Psi.T + np.mat(eye(self.n))+np.multiply(np.mat(eye(self.n)),np.spacing(1))
--- pyKriging/test_matrixops.py
+++ pyKriging/test_matrixops.py
@@ -4,46 +4,67 @@ import pdb
 from geomdl import BSpline
 from geomdl import exchange
 from geomdl.visualization import VisMPL as vis
+import pyKriging
 from pyKriging import matrixops
+from pyKriging import samplingplan 
+from geomdl import utilities
+from pyKriging.regressionkrige import regression_kriging
+from matplotlib import cm
+
 
 class Test_RSM(unittest.TestCase):
         
     def test_RMSD_fun(self):
         
+        num_p = 100
+        # The Kriging model starts by defining a sampling plan, we use an optimal Latin Hypercube here
+        sp = samplingplan(2)  
+        X = sp.rlh(num_p)
+        
+        testfun = pyKriging.testfunctions().branin
+        y = testfun(X)
+        
+        
         # Create a BSpline surface instance
         surf = BSpline.Surface()
 
         # Set degrees
-        surf.degree_u = 3
-        surf.degree_v = 3
+        surf.degree_u = 2
+        surf.degree_v = 2
+        ctrlpts_u = 3
+        ctrlpts_v = 3
 
         # Set control points
         surf.set_ctrlpts(*exchange.import_txt("ex_surface01.cpt", two_dimensional=True))
-
-        # Set knot vectors
-        surf.knotvector_u = [0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0, 3.0]
-        surf.knotvector_v = [0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0, 3.0]
-
+        
+        i_vec = np.linspace(0, 1, num=ctrlpts_u)
+        j_vec = np.linspace(0, 1, num=ctrlpts_v)
+        initial_CP = []  # np.zeros((6, 6, 3))
+        for i in range(0, len(i_vec)):
+            for j in range(0, len(j_vec)):
+                initial_CP.append([i_vec[i], j_vec[j], 20])
+        surf.set_ctrlpts(initial_CP, ctrlpts_u, ctrlpts_v)
+        
+        surf.knotvector_u = utilities.generate_knot_vector(surf.degree_u, ctrlpts_u)
+        surf.knotvector_v = utilities.generate_knot_vector(surf.degree_v, ctrlpts_v)
+        
         # Set evaluation delta
         surf.delta = 0.025
 
         # Evaluate surface points
         surf.evaluate()
 
-        # Import and use Matplotlib's colormaps
-        from matplotlib import cm
-
         # Plot the control point grid and the evaluated surface
         vis_comp = vis.VisSurfTriangle()
         surf.vis = vis_comp
-        surf.render(colormap=cm.coolwarm)
         
+        k = regression_kriging(X, y, testfunction=testfun, name='simple')
         
+        new_s = k.controlPointsOpt(surf, X, y, np.diag(np.ones((len(X),))))
         
-        matrixops.controlPointsOpt(surf.knotvector_u, surf.knotvector_v, surf.ctrlpts, u, v, p, np.diag(np.ones((len(surf.knotvector_u),)))))
+        new_s.evaluate()
+        vis_comp = vis.VisSurfTriangle()
+        # new_s.vis = vis_comp
+        # new_s.render(colormap=cm.coolwarm)
+    
         
-        
-        
-        
-        
-        pdb.set_trace()
